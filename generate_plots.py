b62a2d51abbffd123dcb734c32153eb7608557d5
John Freeman <jlfreeman@fastmail.com>
2024-04-22 22:30:43 -0400

    Add ability to generate plots and stats

--- generate_plots.py
+++ generate_plots.py
@@ -1,83 +1,251 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.ticker import PercentFormatter
+import math
+import scipy
+
+def graphs():
+    datasets = ("PR76", "PR107", "PR124", "PR136", "PR144", "PR152", "PR226", "PR264", "PR439", "PR1002", "PR2392")
+    means = {
+        '0.01': (1,	0.996078431,	1,	1,	1,	1,	1,	0.995420867, 1, 1, 1),
+        '0.05': (1,	1,	1,	1,	1,	1,	1,	1, 1, 1, 1),
+        '0.10': (1,	0.996078431,	1,	1,	1,	1,	1,	0.995420867, 1, 1, 1),
+    }
+
+    x = np.arange(len(datasets))
+    width = 0.25
+    multiplier = 0
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for attribute, measurement in means.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, tick_label="test", label=attribute)
+        multiplier += 1
+    ax.set_title("Normal GA With Different Mutation Rates")
+    ax.legend(loc="lower right")
+    ax.set_xticks(x + width, datasets)
+    ax.set_ylabel("Average Solution Cost")
+    plt.show()
+
+    datasets = ("PR76", "PR107", "PR124", "PR136", "PR144", "PR152", "PR226", "PR264", "PR439", "PR1002", "PR2392")
+    means = {
+        '0.01': (1,	0.996078431,	1,	1,	1,	1,	1,	0.995420867),
+        '0.05': (1,	1,	1,	1,	1,	1,	1,	1),
+        '0.10': (1,	0.996078431,	1,	1,	1,	1,	1,	0.995420867),
+    }
+
+    x = np.arange(len(datasets))
+    width = 0.25
+    multiplier = 0
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for attribute, measurement in means.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, tick_label="test", label=attribute)
+        multiplier += 1
+    ax.set_title("CRISPR GA With Different Mutation Rates")
+    ax.legend(loc="lower right")
+    ax.set_xticks(x + width, datasets)
+    ax.set_ylabel("Average Solution Cost")
+    plt.show()
+
+    datasets = ("PR76", "PR107", "PR124", "PR136", "PR144", "PR152", "PR226", "PR264", "PR439", "PR1002", "PR2392")
+    means = {
+        'Normal GA': (1,	0.996078431,	1,	1,	1,	1,	1,	0.995420867),
+        'CRISPR GA': (1,	1,	1,	1,	1,	1,	1,	1),
+    }
+
+    x = np.arange(len(datasets))
+    width = 0.25
+    multiplier = 0
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    for attribute, measurement in means.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, tick_label="test", label=attribute)
+        multiplier += 1
+    ax.set_title("Normal GA vs CRISPR GA")
+    ax.legend(loc="lower right")
+    ax.set_xticks(x + width, datasets)
+    ax.set_ylabel("Average Solution Cost / Optimal Cost")
+    plt.show()
+
+
+    cities = [76, 107, 124, 136, 144, 152, 226, 264, 439, 1002, 2392]
+    ga_times = [76, 107, 124, 136, 144, 152, 226, 264, 439, 1002, 2392]
+    crispr_times = [76, 107, 124, 136, 144, 152, 226, 264, 439, 1002, 2392]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(cities, ga_times, color='tab:blue', label='Normal GA')
+    ax.plot(cities, crispr_times, color='tab:red', label='CRISPR GA')
+    ax.set_title("Normal GA vs CRISPR GA")
+    ax.legend()
+    ax.ticklabel_format(style="plain")
+    ax.set_xlabel("# of Cities")
+    ax.set_ylabel("Average Runtime in Milliseconds")
+    plt.show()
+
+def stats():
+
+    print("PR76")
+
+    pr76_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr76_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr76_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr76_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr76_ga_costs, pr76_crispr_costs))
+
+    print("\nPR107")
+
+    pr107_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr107_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr107_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr107_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr107_ga_costs, pr107_crispr_costs))
+
+    print("\nPR124")
+
+    pr124_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr124_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr124_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr124_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr124_ga_costs, pr124_crispr_costs))
+
+    print("\nPR136")
+
+    pr136_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr136_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr136_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr136_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr136_ga_costs, pr136_crispr_costs))
+
+    print("\nPR144")
+
+    pr144_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr144_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr144_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr144_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr144_ga_costs, pr144_crispr_costs))
+
+    print("\nPR152")
+
+    pr152_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr152_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr152_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr152_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr152_ga_costs, pr152_crispr_costs))
+
+    print("\nPR226")
+
+    pr226_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr226_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr226_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr226_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr226_ga_costs, pr226_crispr_costs))
+
+    print("\nPR264")
+
+    pr264_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr264_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr264_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr264_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr264_ga_costs, pr264_crispr_costs))
+
+    print("\nPR439")
+
+    pr439_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr439_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr439_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr439_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr439_ga_costs, pr439_crispr_costs))
+
+    print("\nPR1002")
+
+    pr1002_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr1002_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr1002_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr1002_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr1002_ga_costs, pr1002_crispr_costs))
+
+    print("\nPR2392")
+
+    pr2392_ga_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+    pr2392_crispr_costs = [309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309, 309]
+
+    print("GA Median")
+    print(np.var(pr2392_ga_costs))
+
+    print("CRISPR Median")
+    print(np.var(pr2392_crispr_costs))
+
+    print("GA vs CRISPR")
+    print("%.10f (%.10f)" % scipy.stats.kruskal(pr2392_ga_costs, pr2392_crispr_costs))
+
 
 if __name__ == '__main__':
-    number_of_items = [5, 6, 7, 7, 8, 10, 15, 24]
-    average_accuracy = [1 - ((51.0 - 51.0) / 51.0), 1 - ((150.0 - 150.0) / 150.0), 1 - ((107.0 - 107.0) / 107.0), 1 - ((1735.0 - 1735.0) / 1735.0), 1 - ((900.0 - 900.0) / 900.0), 1 - ((309.0 - 309.0) / 309.0), 1 - ((1458.0 - 1457.9) / 1458.0), 1 - ((13549094.0 - 13535570.8) / 13549094.0)]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(number_of_items, average_accuracy, color='tab:blue')
-    ax.set_title("Knapsack")
-    ax.ticklabel_format(style="plain")
-    ax.set_xticks(number_of_items)
-    ax.set_xlabel("# of Items")
-    ax.yaxis.set_major_formatter(PercentFormatter(1))
-    ax.set_ylabel("Average Accuracy")
-    plt.show()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(number_of_items, average_accuracy, color='tab:blue')
-    ax.set_title("Knapsack")
-    ax.ticklabel_format(style="plain")
-    ax.set_xticks(number_of_items)
-    ax.set_xlabel("# of Items")
-    ax.set_ybound(0, 1.1)
-    ax.yaxis.set_major_formatter(PercentFormatter(1))
-    ax.set_ylabel("Average Accuracy")
-    plt.show()
-
-    sizes = [5, 10, 15, 20, 23]
-    dp_times = [0.049850000000000005, 5.977449999999999, 497.2015, 35524.901600000005, 430728.2624]
-    ga_times = [2972.34775, 4886.674499999999, 6870.810650000001, 8826.12165, 10029.394900000001]
-    dp_costs = [2.0379680084182166, 2.74116754204749, 3.064577470394665, 3.619913176312962, 3.9900946485758024]
-    ga_costs = [2.0379680084182175, 2.7411675420474904, 3.0645774703946653, 3.619913176312962, 3.990094648575804]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(sizes, ga_times, color='tab:blue', label='GA')
-    ax.plot(sizes, dp_times, color='tab:red', label='Held-Karp')
-    ax.set_title("TSP")
-    ax.legend()
-    ax.ticklabel_format(style="plain")
-    ax.set_xticks(sizes)
-    ax.set_xlabel("# of Cities")
-    ax.set_ylabel("Runtime in milliseconds")
-    plt.show()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(sizes, ga_costs, color='tab:blue', label='GA')
-    ax.plot(sizes, dp_costs, color='tab:red', label='Held-Karp')
-    ax.set_title("TSP")
-    ax.legend()
-    ax.ticklabel_format(style="plain")
-    ax.set_xticks(sizes)
-    ax.set_xlabel("# of Cities")
-    ax.set_ylabel("Solution Cost")
-    plt.show()
-
-    sizes = list(range(10, 110, 10))
-    times = [4839.832450000001, 8751.881149999997, 12653.628550000001, 16474.67735, 20277.3613, 24367.35055, 28365.61965, 32403.979049999994, 36405.860349999995, 41303.83255]
-    costs = [2.7411675420474904, 3.619913176312962, 4.710520377882223, 5.046832865615033, 5.600194513914297, 6.015611021694051, 6.535803406737341, 6.777443402026033, 7.3791734380163945, 8.102403661169118]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(sizes, times, color='tab:blue')
-    ax.set_title("TSP with GA")
-    ax.ticklabel_format(style="plain")
-    ax.set_xticks(sizes)
-    ax.set_xlabel("# of Cities")
-    ax.set_ylabel("Runtime in milliseconds")
-    plt.show()
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(sizes, costs, color='tab:blue')
-    ax.set_title("TSP with GA")
-    ax.ticklabel_format(style="plain")
-    ax.set_xticks(sizes)
-    ax.set_xlabel("# of Cities")
-    ax.set_ylabel("Solution Cost")
-    plt.show()
+    graphs()
+    stats()
